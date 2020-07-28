30b9f5b40a45bb7ad3fdd20a8f37bd99e70680f9
Samuel Walker <sfxworks@gmail.com>
2020-07-27 20:27:43 -0400

    Add database deployment type. Add web directory to minecraft directory.

--- package.ps1
+++ package.ps1
@@ -3,6 +3,7 @@ helm package .\sftp-deployment\ -d .\releases
 helm package .\namespace-lock\ -d .\releases
 helm package .\bungee-deployment\ -d .\releases
 helm package .\backup-deployment\ -d .\releases
+helm package .\database-deployment\ -d .\releases
 helm repo index .\releases\ --url https://mcserverhosting-net.github.io/charts/releases
 rm .\index.yaml
 mv .\releases\index.yaml .
