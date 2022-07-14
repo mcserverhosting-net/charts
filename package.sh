12ee70dd84206b05d4996b38c2a779b1c841de24
Samuel Walker <services@sfxworks.net>
2022-07-14 06:38:46 -0400

    chart update

---
+++ package.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+helm package server-deployment -d releases
+helm package mcsh-customer -d releases
+helm package sftp-deployment -d releases
+helm package namespace-lock -d releases
+helm package bungee-deployment -d releases
+helm package backup-deployment -d releases
+helm package database-deployment -d releases
+helm repo index releases --url "https://mcserverhosting-net.github.io/charts/releases"
+rm index.yaml
+mv releases/index.yaml .
